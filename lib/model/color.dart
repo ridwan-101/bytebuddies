b804d6cad9e5a45d7f830d4c900b70dab53c8da8
Ridwan <abdulazeezridwan004@gmail.com>
2023-10-12 11:24:35 +0100

    first commit

---
+++ lib/model/color.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const Color kcPrimaryColor = Color(0xFF9600FF);
+const Color kcPrimaryColorDark = Color(0xFF300151);
+const Color kcDarkGreyColor = Color(0xFF1A1B1E);
+const Color kcMediumGrey = Color(0xFF474A54);
+const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
+const Color kcVeryLightGrey = Color(0xFFE3E3E3);
+const Color kcBackgroundColor = kcDarkGreyColor;
+
+class AppColors {
+  static Color primary = const Color(0xff85B6FF);
+  static Color black = const Color(0xff0F0F0F);
+  static Color white = const Color(0xffFCFCFC);
+  static Color secondary = const Color(0xff0047B3);
+  static Color red = const Color(0xffA81400);
+  static Color green = const Color(0xff95E574);
+  static Color blackLightActive = const Color(0xffB5B5B5);
+}
