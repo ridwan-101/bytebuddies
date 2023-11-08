29bcd7ddd15086cf89de44dc9b11d24dd843342f
Ridwan <abdulazeezridwan004@gmail.com>
2023-11-08 12:20:02 +0100

    bottom navgator

--- lib/ui/views/bottom_navigator/bottom_navigator_view.dart
+++ lib/ui/views/bottom_navigator/bottom_navigator_view.dart
@@ -12,7 +12,12 @@ class BottomBarMenu extends StatefulWidget {
 
 class _BottomBarMenuState extends State<BottomBarMenu> {
   int index = 0;
-  final Screens = [HomeView(), Wallet(), Chatbot(), Notifier()];
+  final Screens = [
+    const HomeView(),
+    const Wallet(),
+    const Chatbot(),
+    const Notifier()
+  ];
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -21,32 +26,46 @@ class _BottomBarMenuState extends State<BottomBarMenu> {
           data: NavigationBarThemeData(
               indicatorColor: AppColors.seaGreen,
               labelTextStyle: MaterialStateProperty.all(
-                  TextStyle(fontWeight: FontWeight.w900, fontSize: 14)),
+                  const TextStyle(fontWeight: FontWeight.w900, fontSize: 14)),
               labelBehavior: NavigationDestinationLabelBehavior.alwaysHide),
           child: NavigationBar(
             height: 60,
             selectedIndex: index,
             backgroundColor: AppColors.white,
-            animationDuration: Duration(seconds: 2),
+            animationDuration: const Duration(seconds: 2),
             onDestinationSelected: (index) =>
                 setState(() => this.index = index),
             destinations: const [
               NavigationDestination(
-                icon: Icon(Icons.home_outlined, ),
-                selectedIcon: Icon(Icons.home_outlined, ),
+                icon: Icon(
+                  Icons.home_outlined,
+                ),
+                selectedIcon: Icon(
+                  Icons.home_outlined,
+                  color: Colors.white,
+                ),
                 label: 'Home',
               ),
               NavigationDestination(
                   icon: Icon(Icons.account_balance_wallet),
-                  selectedIcon: Icon(Icons.account_balance_wallet),
+                  selectedIcon: Icon(
+                    Icons.account_balance_wallet,
+                    color: Colors.white,
+                  ),
                   label: 'Wallet'),
               NavigationDestination(
                   icon: Icon(Icons.support_agent_sharp),
-                  selectedIcon: Icon(Icons.support_agent_sharp),
+                  selectedIcon: Icon(
+                    Icons.support_agent_sharp,
+                    color: Colors.white,
+                  ),
                   label: 'Support'),
               NavigationDestination(
                   icon: Icon(Icons.notifications),
-                  selectedIcon: Icon(Icons.notifications),
+                  selectedIcon: Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+                  ),
                   label: 'Settings'),
             ],
           ),
